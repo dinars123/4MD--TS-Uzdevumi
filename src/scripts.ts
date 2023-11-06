273282c9d095b9874af5e24be944eeaf6e94a757
dinars123 <129218944+dinars123@users.noreply.github.com>
2023-11-06 23:06:56 +0200

    commit

--- src/scripts.ts
+++ src/scripts.ts
@@ -6,3 +6,7 @@ console.log('Ready for coding');
 console.log('Body jQuery node:', $('body'));
 console.log('Body javascript node:', document.querySelector('body'));
 console.log('2 + 3 =', sum(2, 3));
+/*
+*/
+
+console.log('a')
